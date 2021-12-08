8a23eb9f8f34e8afaea465113658b99cdd1dd233
Daria Mordvinov <71291161+DariaMordvinov@users.noreply.github.com>
2021-12-07 23:17:43 -0500

    Add files via upload

---
+++ Shell Sort/shellSort.py
@@ -0,0 +1,18 @@
+def shellSort(array):
+    n = len(array)
+    interval = n // 2
+    while interval > 0:
+        for i in range(interval, n):
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+
+            array[j] = temp
+        interval //= 2
+
+
+array = [9, 8, 3, 7, 5, 6, 4, 1]
+shellSort(array)
+print(array)
